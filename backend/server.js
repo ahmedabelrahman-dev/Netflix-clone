c8db7a5a35e4966a5d9645493066b56dd2294abf
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-06 13:30:07 +0400

    feat: implement server setup with Express and initial middleware

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+
+import authRoutes from './routes/auth.route.js';
+import { ENV_VARS } from './config/envVars.js';
+
+import { connectDB } from './config/db.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = ENV_VARS.PORT || 5000;
+
+app.use(express.json()); // will allow us to parse req.body
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api/v1/auth', authRoutes);
+
+app.listen(PORT, () => {
+  console.log('Server started at http://localhost:' + PORT);
+  connectDB();
+});
