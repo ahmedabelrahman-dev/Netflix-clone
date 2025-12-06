42297094106db99a80ed15c3495a24fa11a6b1b8
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-06 16:35:17 +0400

    feat: add TV routes with protection to server configuration

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import bodyParser from 'body-parser';
 
 import authRoutes from './routes/auth.route.js';
 import movieRoutes from './routes/movie.route.js';
+import tvRoutes from './routes/tv.route.js';
 import { protectRoute } from './middleware/protectRoute.js';
 
 import { ENV_VARS } from './config/envVars.js';
@@ -21,6 +22,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/movie', protectRoute, movieRoutes);
+app.use('/api/v1/tv', protectRoute, tvRoutes);
 
 app.listen(PORT, () => {
   console.log('Server started at http://localhost:' + PORT);
