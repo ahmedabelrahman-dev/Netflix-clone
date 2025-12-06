a0350ca835cc18306cab1d8c05eeeac91087f237
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-06 16:22:27 +0400

    feat: implement movie controller functions for trending movies, trailers, details, similar movies, and category-based retrieval

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,10 @@ import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 
 import authRoutes from './routes/auth.route.js';
-import { ENV_VARS } from './config/envVars.js';
+import movieRoutes from './routes/movie.route.js';
+import { protectRoute } from './middleware/protectRoute.js';
 
+import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
 
 dotenv.config();
@@ -16,7 +18,9 @@ const PORT = ENV_VARS.PORT || 5000;
 app.use(express.json()); // will allow us to parse req.body
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/movie', protectRoute, movieRoutes);
 
 app.listen(PORT, () => {
   console.log('Server started at http://localhost:' + PORT);
