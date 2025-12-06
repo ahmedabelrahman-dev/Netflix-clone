2095a785d80f5227766fa92fd7aab74b34052b11
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-06 14:34:35 +0400

    feat: add movie controller and routes for trending movies, trailers, details, similar movies, and category-based retrieval

---
+++ backend/controllers/movie.controller.js
@@ -0,0 +1,9 @@
+export async function getTrendingMovie(req, res) {}
+
+export async function getMovieTrailers(req, res) {}
+
+export async function getMovieDetails(req, res) {}
+
+export async function getSimilarMovies(req, res) {}
+
+export async function getMoviesByCategory(req, res) {}
