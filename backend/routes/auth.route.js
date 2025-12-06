a296ed7160ce11c5150fe2fff372601db7c5934d
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-06 13:41:29 +0400

    feat: add authCheck route for user authentication status verification

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,17 @@
 import express from 'express';
-import { login, signup, logout } from '../controllers/auth.controller.js';
+import {
+  login,
+  signup,
+  logout,
+  authCheck,
+} from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/signup', signup);
 router.post('/logout', logout);
+router.get('/authCheck', protectRoute, authCheck);
 
 export default router;
