c5b7c505a5942449998aee510fe1c63e54dabf72
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-06 13:30:53 +0400

    feat: implement signup, login, and logout functionality in auth controller

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,84 @@
+import { User } from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { generateTokenAndSetCookie } from '../utils/generateToken.js';
+
+export async function signup(req, res) {
+  try {
+    const { email, password, username } = req.body;
+    // Basic validation
+    if (!email || !password || !username) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'All fields are required' });
+    }
+    // Email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ success: false, message: 'Invalid email' });
+    }
+    // Password validation
+    if (password.length < 6) {
+      return res.status(400).json({
+        success: false,
+        message: 'Password must be at least 6 characters',
+      });
+    }
+
+    const existingUserByEmail = await User.findOne({ email: email });
+    // Check if user with the same email already exists
+    if (existingUserByEmail) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Email already exists' });
+    }
+    // Check if user with the same username already exists
+    const existingUserByUsername = await User.findOne({ username: username });
+
+    if (existingUserByUsername) {
+      return res
+        .status(400)
+        .json({ success: false, message: 'Username already exists' });
+    }
+    // Hash the password
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const PROFILE_PICS = ['/avatar1.png', '/avatar2.png', '/avatar3.png'];
+
+    const image = PROFILE_PICS[Math.floor(Math.random() * PROFILE_PICS.length)];
+
+    // Create a new user
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+      username,
+      image,
+    });
+
+    // Generate JWT token and set it in HTTP-only cookie
+    generateTokenAndSetCookie(newUser._id, res);
+    await newUser.save();
+
+    res.status(201).json({
+      success: true,
+      user: {
+        ...newUser._doc,
+        password: '',
+      },
+    });
+  } catch (error) {
+    console.log('Error in signup controller', error.message);
+    res.status(500).json({ success: false, message: 'Internal server error' });
+  }
+}
+
+export async function login(req, res) {
+  // Handle user login
+  res.send('Login route');
+}
+
+export async function logout(req, res) {
+  // Handle user logout
+  res.send('logout route');
+}
