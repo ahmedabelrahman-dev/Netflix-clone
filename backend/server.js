9e8d203ba8a66bae34cdbc6e0c7a6eb74d114686
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-07 14:09:20 +0400

    feat: added search routes and controllers for searching for movie/tv show/person

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,9 @@ import bodyParser from 'body-parser';
 import authRoutes from './routes/auth.route.js';
 import movieRoutes from './routes/movie.route.js';
 import tvRoutes from './routes/tv.route.js';
-import { protectRoute } from './middleware/protectRoute.js';
+import searchRoutes from './routes/search.route.js';
 
+import { protectRoute } from './middleware/protectRoute.js';
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
 
@@ -23,6 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/movie', protectRoute, movieRoutes);
 app.use('/api/v1/tv', protectRoute, tvRoutes);
+app.use('/api/v1/search', protectRoute, searchRoutes);
 
 app.listen(PORT, () => {
   console.log('Server started at http://localhost:' + PORT);
