f128f996a1eed5387ec7e75a90cc342973a4f5a9
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-07 15:39:10 +0400

    feat(page): test home page , check the user authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,39 @@ import SignUpPage from './pages/SignUpPage.jsx';
 import NotFoundPage from './pages/404.jsx';
 import Footer from './components/Footer.jsx';
 import { Toaster } from 'react-hot-toast';
+import { useAuthStore } from './store/authUser.js';
+import { Navigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Loader } from 'lucide-react';
 
 function App() {
+  const { user, isCheckingAuth, authCheck } = useAuthStore();
+
+  useEffect(() => {
+    authCheck();
+  }, [authCheck]);
+
+  if (isCheckingAuth) {
+    return (
+      <div className="h-screen">
+        <div className="flex justify-center items-center bg-black h-full">
+          <Loader className="animate-spin text-red-600 size-10" />
+        </div>
+      </div>
+    );
+  }
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route
+          path="/login"
+          element={!user ? <LoginPage /> : <Navigate to={'/'} />}
+        />
+        <Route
+          path="/signup"
+          element={!user ? <SignUpPage /> : <Navigate to={'/'} />}
+        />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <Footer />
