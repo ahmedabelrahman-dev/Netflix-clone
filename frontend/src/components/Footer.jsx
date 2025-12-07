ad55a50d2b4ecad46cf6b942c04cb59c7ec7b8bf
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-07 15:37:04 +0400

    updated the footer component with link to github

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <footer className="py-6 md:px-8 md:py-0 bg-black text-white border-t border-gray-800">
       <div className="flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
-          Built by{'Ahned Abelrahman'} for{'  '}
+          Built by {' Ahmed Abelrahman '}. Feel free to reach out to{' '}
           <a
             href="https://github.com/ahmedabelrahman-dev"
             target="_blank"
