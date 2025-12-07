8af53acfb7a29fba7c1a062a133e15c8db637bd5
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-07 23:35:07 +0400

    feat(components): added loading skeleton , footer, navigation bar, movie slider to navigate between movies/tv shows

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,13 +3,13 @@ const Footer = () => {
     <footer className="py-6 md:px-8 md:py-0 bg-black text-white border-t border-gray-800">
       <div className="flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
         <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
-          Built by {' Ahmed Abelrahman '}. Feel free to reach out to{' '}
+          Built by{' '}
           <a
             href="https://github.com/ahmedabelrahman-dev"
             target="_blank"
             className="font-medium underline underline-offset-4"
           >
-            you
+            Ahmed Abelrahman
           </a>
           . The source code is available on{' '}
           <a
